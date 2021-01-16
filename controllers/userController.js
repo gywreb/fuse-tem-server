1be86b5385799bafcef062ffd7deebb1be8b9149
gywreb <longnguyenhoang91@gmail.com>
2021-01-16 23:02:34 +0700

    create get all user api

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../database/models/User");
 const asyncMiddleware = require("../middlewares/asyncMiddleware");
+const ErrorResponse = require("../models/ErrorResponse");
 
 exports.createNewUser = asyncMiddleware(async (req, res, next) => {
   const { password, role, displayName, email } = req.body;
@@ -14,4 +15,10 @@ exports.createNewUser = asyncMiddleware(async (req, res, next) => {
   });
   const newUser = await user.save();
   res.status(200).json(newUser);
+});
+
+exports.getAll = asyncMiddleware(async (req, res, next) => {
+  const users = await User.find().select("-password");
+  if (!users) return next(new ErrorResponse(404, "no user found"));
+  res.status(200).json(users);
 });
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,9 @@ const upload = require("../middlewares/upload");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.route("/").post(upload.single("photoUrl"), userController.createNewUser);
+router
+  .route("/")
+  .get(userController.getAll)
+  .post(upload.single("photoUrl"), userController.createNewUser);
 
 module.exports = router;
